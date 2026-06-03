9987222bdd3ec70a40b77b5d10da0962184ec65f
Loic Rouaud <42077711+MrBroma@users.noreply.github.com>
2026-06-03 21:21:30 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 36.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Hello"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
